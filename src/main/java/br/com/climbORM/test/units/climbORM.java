9a71ca6e43d13e362b8f22930b1f96bd18181822
bobboyms <bobboyms@gmail.com>
2019-12-27 14:40:24 -0400

    update

--- src/main/java/br/com/climbORM/test/units/climbORM.java
+++ src/main/java/br/com/climbORM/test/units/climbORM.java
@@ -8,11 +8,16 @@ import br.com.climbORM.test.model.Email;
 import br.com.climbORM.test.model.Endereco;
 import br.com.climbORM.test.model.Pessoa;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class climbORM {
 
     public static Long idPessoa;
@@ -20,6 +25,7 @@ public class climbORM {
     public static Long idEndereco;
 
     @Test
+    @Order(1)
     void insert() {
 
         ManagerFactory factory = ClimbORM.createManagerFactory("climb.properties");
@@ -31,14 +37,16 @@ public class climbORM {
         cidade.setNomeDaCidade("Cacoal");
         connection.save(cidade);
 
-        assertTrue(cidade.getId() != null);
+        idCidade = cidade.getId();
+        assertTrue(idCidade != null);
 
         Endereco endereco = new Endereco();
         endereco.setNomeDaRua("Rua Ji-parana");
         endereco.setCidade(cidade);
         connection.save(endereco);
 
-        assertTrue(endereco.getId() != null);
+        idEndereco = endereco.getId();
+        assertTrue(idEndereco != null);
 
         Pessoa pessoa = new Pessoa();
         pessoa.setNome("Maria antonia");
@@ -62,14 +70,17 @@ public class climbORM {
         assertTrue(idPessoa != null);
 
         connection.getTransaction().commit();
+        connection.close();
     }
 
     @Test
+    @Order(2)
     void printaID() {
         System.out.println("ID PESSOA: " + idPessoa);
     }
 
     @Test
+    @Order(3)
     void select() {
         ManagerFactory factory = ClimbORM.createManagerFactory("climb.properties");
         ClimbConnection connection = factory.getConnection("localhost");
@@ -101,8 +112,12 @@ public class climbORM {
             assertTrue(pessoa1.getEndereco().getCidade().getId() != null);
         }
 
+        connection.close();
+
     }
 
+    @Test
+    @Order(4)
     void update() {
 
         ManagerFactory factory = ClimbORM.createManagerFactory("climb.properties");
@@ -110,39 +125,78 @@ public class climbORM {
 
         connection.getTransaction().start();
 
-        Cidade cidade = new Cidade();
-        cidade.setNomeDaCidade("Cacoal");
-        connection.save(cidade);
+        Cidade cidade = (Cidade) connection.findOne(Cidade.class, idCidade);
+        cidade.setNomeDaCidade("Jipa");
+        connection.update(cidade);
+        assertTrue(cidade.getId() == idCidade);
 
-        assertTrue(cidade.getId() != null);
+        Endereco endereco = (Endereco) connection.findOne(Endereco.class, idEndereco);
+        endereco.setNomeDaRua("Rua Taliba");
+        connection.update(endereco);
+        assertTrue(endereco.getId() == idEndereco);
 
-        Endereco endereco = new Endereco();
-        endereco.setNomeDaRua("Rua Ji-parana");
-        endereco.setCidade(cidade);
-        connection.save(endereco);
+        Pessoa pessoa = (Pessoa) connection.findOne(Pessoa.class, idPessoa);
+        pessoa.setNome("Maria update");
+        pessoa.setEnderecoComercial("update");
 
-        assertTrue(endereco.getId() != null);
-
-        Pessoa pessoa = new Pessoa();
-        pessoa.setNome("Maria antonia");
-        pessoa.setEnderecoComercial("Treta");
-        pessoa.setIdade(32l);
-        pessoa.setAltura(174.1325525544f);
-        pessoa.setQtdQuilos(145.6144d);
-        pessoa.setCasado(true);
-        pessoa.setEndereco(endereco);
-        pessoa.setFoto(new byte[] {});
-
-        List<Email> emails = new ArrayList<>();
         Email email = new Email();
-        email.setEmail("taliba@jose.com.br");
-        emails.add(email);
-        pessoa.setEmails(emails);
+        email.setEmail("update@update.com.br");
+        pessoa.getEmails().add(email);
+        connection.update(pessoa);
 
-        connection.save(pessoa);
+        System.out.println("Pessoa id: " + idPessoa);
+        System.out.println("Pessoa id: " + pessoa.getId());
 
-        idPessoa = pessoa.getId();
-        assertTrue(idPessoa != null);
+        assertTrue(pessoa.getId().equals(idPessoa));
+
+        connection.getTransaction().commit();
+        connection.close();
+
+        connection = factory.getConnection("localhost");
+
+        pessoa = (Pessoa) connection.findOne(Pessoa.class, idPessoa);
+
+        System.out.println("size: " + pessoa.getEmails().size());
+
+        assertTrue(pessoa != null);
+        assertTrue(pessoa.getEmails().size() == 2);
+        assertTrue(pessoa.getId().equals(idPessoa));
+        assertTrue(pessoa.getNome().equals("Maria update"));
+        assertTrue(pessoa.getIdade().equals(32l));
+        assertTrue(pessoa.getAltura().equals(174.1325525544f));
+
+        assertTrue(pessoa.getEnderecoComercial().equals("update"));
+        assertTrue(pessoa.getEndereco() != null);
+        assertTrue(pessoa.getEndereco().getId().equals(idEndereco));
+        assertTrue(pessoa.getEndereco().getNomeDaRua().equals("Rua Taliba"));
+
+        assertTrue(pessoa.getEndereco().getCidade() != null);
+        assertTrue(pessoa.getEndereco().getCidade().getId().equals(idCidade));
+        assertTrue(pessoa.getEndereco().getCidade().getNomeDaCidade().equals("Jipa"));
+
+        connection.close();
+
+    }
+
+    @Test
+    @Order(5)
+    void deleteAllBase() {
+        ManagerFactory factory = ClimbORM.createManagerFactory("climb.properties");
+        ClimbConnection connection = factory.getConnection("localhost");
+
+        connection.getTransaction().start();
+        connection.delete(Pessoa.class, "where id = " + idPessoa.toString());
+        connection.delete(Cidade.class, "where id = " + idCidade.toString());
+        connection.delete(Endereco.class, "where id = " + idEndereco.toString());
+
+        connection.getTransaction().commit();
+        connection.close();
+
+        connection = factory.getConnection("localhost");
+
+        assertTrue(connection.findOne(Pessoa.class, idPessoa) == null);
+        assertTrue(connection.findOne(Cidade.class, idCidade) == null);
+        assertTrue(connection.findOne(Endereco.class, idEndereco) == null);
 
     }
 
