b0dfb6ed37cc9051d9de2410a0dc9b99d6763813
bobboyms <bobboyms@gmail.com>
2019-12-30 18:13:15 -0400

    update

--- src/main/java/br/com/climbORM/framework/utils/SqlUtil.java
+++ src/main/java/br/com/climbORM/framework/utils/SqlUtil.java
@@ -5,13 +5,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.beans.PropertyDescriptor;
 import java.sql.*;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class SqlUtil {
 
-    public synchronized static byte[] getBinaryValue(Long id, String field, String entity, Connection connection)
+    public static byte[] getBinaryValue(Long id, String field, String entity, Connection connection)
             throws SQLException {
         Statement stmt = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, 1);
         ResultSet resultSet = stmt.executeQuery("SELECT " + field + " FROM " + entity + " WHERE ID = " + id.toString());
@@ -23,17 +21,17 @@ public class SqlUtil {
         return null;
     }
 
-    public synchronized static PreparedStatement preparedStatementInsert(String schema, Connection connection, List<ModelTableField> modelTableFields,
+    public static PreparedStatement preparedStatementInsert(String schema, Connection connection, List<Model> models,
                                                             String tableName) throws Exception {
 
         StringBuilder atributes = new StringBuilder();
         StringBuilder values = new StringBuilder();
+        for (Model model : models) {
 
-        for (ModelTableField modelTableField : modelTableFields) {
-
-            atributes.append(modelTableField.getAttribute() + ",");
-            if (modelTableField.getField().isAnnotationPresent(Json.class)) {
-                values.append("?::JSON,");
+            atributes.append(model.getAttribute() + ",");
+            if (model.getField().isAnnotationPresent(Json.class)) {
+                Json json = (Json)model.getField().getAnnotation(Json.class);
+                values.append("?::"+json.typeJson()+",");
             } else {
                 values.append("?,");
             }
@@ -47,108 +45,40 @@ public class SqlUtil {
 
         PreparedStatement ps = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
 
-        return getPreparedStatement(ps, modelTableFields);
+        return getPreparedStatement(ps, models);
 
     }
 
-    public synchronized static PreparedStatement getPreparedStatementDynamicFields(PreparedStatement ps, Map<String, Object> fields, int start) throws Exception {
-
-        int i = start;
-
-        for (String fieldName : fields.keySet()) {
-
-            Object object = fields.get(fieldName);
-
-            i += 1;
-            if (object.getClass() == Long.class || object.getClass() == long.class) {
-                ps.setLong(i, (Long) object);
-            } else if (object.getClass() == Integer.class || object.getClass() == int.class) {
-                ps.setInt(i, (int) object);
-            } else if (object.getClass() == Float.class || object.getClass() == float.class) {
-                ps.setFloat(i, (float) object);
-            } else if (object.getClass() == Double.class || object.getClass() == double.class) {
-                ps.setDouble(i, (double) object);
-            } else if (object.getClass() == Boolean.class || object.getClass() == boolean.class) {
-                ps.setBoolean(i, (boolean) object);
-            } else if (object.getClass() == String.class || object.getClass() == char.class) {
-                ps.setString(i, (String) object);
-            } else if (object.getClass() == byte[].class) {
-                ps.setBytes(i, (byte[]) object);
-            }
-        }
-
-        return ps;
-    }
-
-    public synchronized static PreparedStatement getPreparedStatement(PreparedStatement ps, List<ModelTableField> modelTableFields) throws Exception {
+    public static PreparedStatement getPreparedStatement(PreparedStatement ps, List<Model> models) throws Exception {
 
         int i = 0;
-        for (ModelTableField modelTableField : modelTableFields) {
+        for (Model model : models) {
 
             i += 1;
-            if (modelTableField.getType() == Long.class || modelTableField.getType() == long.class) {
-                if (ReflectionUtil.isProxedCGLIB(modelTableField.getValue())) {
+            if (model.getType() == Long.class || model.getType() == long.class) {
+                if (ReflectionUtil.isProxedCGLIB(model.getValue())) {
 
-                    Object value = new PropertyDescriptor("id", modelTableField.getValue().getClass().getSuperclass()).getReadMethod().invoke(modelTableField.getValue());
+                    Object value = new PropertyDescriptor("id", model.getValue().getClass().getSuperclass()).getReadMethod().invoke(model.getValue());
                     ps.setLong(i, (long) value);
 
                 } else {
-                    if (modelTableField.getValue() != null) {
-                        ps.setLong(i, (long) modelTableField.getValue());
-                    } else {
-                        ps.setNull(i, 0);
-                    }
+                    ps.setLong(i, (long) model.getValue());
                 }
-            } else if (modelTableField.getType() == Integer.class || modelTableField.getType() == int.class) {
-
-                if (modelTableField.getValue() == null) {
-                    ps.setNull(i, 0);
-                } else {
-                    ps.setInt(i, (int) modelTableField.getValue());
-                }
-
-            } else if (modelTableField.getType() == Float.class || modelTableField.getType() == float.class) {
-                if (modelTableField.getValue() == null) {
-                    ps.setNull(i,0);
-                } else {
-                    ps.setFloat(i, (float) modelTableField.getValue());
-                }
-
-            } else if (modelTableField.getType() == Double.class || modelTableField.getType() == double.class) {
-
-                if (modelTableField.getValue() == null) {
-                    ps.setNull(i, 0);
-                } else {
-                    ps.setDouble(i, (double) modelTableField.getValue());
-                }
-
-            } else if (modelTableField.getType() == Boolean.class || modelTableField.getType() == boolean.class) {
-
-                if (modelTableField.getValue() == null) {
-                    ps.setNull(i, 0);
-                } else {
-                    ps.setBoolean(i, (boolean) modelTableField.getValue());
-                }
-
-            } else if (modelTableField.getType() == String.class || modelTableField.getType() == char.class) {
-
-                if (modelTableField.getValue() == null) {
-                    ps.setNull(i, 0);
-                } else {
-                    ps.setString(i, modelTableField.getValue().toString());
-                }
-
-            } else if (modelTableField.getType() == byte[].class) {
-
-                if (modelTableField.getValue() == null) {
-                    ps.setNull(i, 0);
-                } else {
-                    ps.setBytes(i, (byte[]) modelTableField.getValue());
-                }
-
-            } else if (modelTableField.getType() == List.class && modelTableField.getField().isAnnotationPresent(Json.class)) {
+            } else if (model.getType() == Integer.class || model.getType() == int.class) {
+                ps.setInt(i, (int) model.getValue());
+            } else if (model.getType() == Float.class || model.getType() == float.class) {
+                ps.setFloat(i, (float) model.getValue());
+            } else if (model.getType() == Double.class || model.getType() == double.class) {
+                ps.setDouble(i, (double) model.getValue());
+            } else if (model.getType() == Boolean.class || model.getType() == boolean.class) {
+                ps.setBoolean(i, (boolean) model.getValue());
+            } else if (model.getType() == String.class || model.getType() == char.class) {
+                ps.setString(i, model.getValue().toString());
+            } else if (model.getType() == byte[].class) {
+                ps.setBytes(i, (byte[]) model.getValue());
+            } else if (model.getType() == List.class && model.getField().isAnnotationPresent(Json.class)) {
                 ObjectMapper mapper = new ObjectMapper();
-                String jsonString = mapper.writeValueAsString(modelTableField.getValue());
+                String jsonString = mapper.writeValueAsString(model.getValue());
                 ps.setObject(i, jsonString);
             }
         }
@@ -156,94 +86,19 @@ public class SqlUtil {
         return ps;
     }
 
-    public synchronized static Map<String,String> getCreatedFields(final String tableName, final Connection connection) {
-
-        final String sql = "SELECT column_name, data_type FROM information_schema.columns WHERE \n" +
-                "table_name = ?;";
-
-        Map<String, String> map = null;
-        PreparedStatement ps = null;
-        try {
-
-            map = new HashMap<>();
-
-            ps = connection.prepareStatement(sql);
-            ps.setString(1, tableName);
-
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                map.put(rs.getString("column_name"), rs.getString("data_type"));
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                ps.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-
-        return map;
-
-
-    }
-
-    public synchronized static String getTypeDataBase(final Class type) {
-
-        if (type == Long.class || type == long.class) {
-            return "integer";
-        } else if (type == Integer.class || type == int.class) {
-            return "integer";
-        } else if (type == Float.class || type == float.class) {
-            return "decimal";
-        } else if (type == Double.class || type == double.class) {
-            return "real";
-        } else if (type == Boolean.class || type == boolean.class) {
-            return "boolean";
-        } else if (type == String.class || type == char.class) {
-            return "text";
-        } else if (type == byte[].class) {
-            return "bytea";
-        } else {
-            new Error("Not suportend type: " + type);
-        }
-
-        return null;
-    }
-
-    public synchronized static Class getTypeDataJava(final String type) {
-
-        if ("integer".equals(type)) {
-            return Long.class;
-        } else if ("decimal".equals(type)) {
-            return Float.class;
-        } else if ("real".equals(type)) {
-            return Double.class;
-        } else if ("boolean".equals(type)) {
-            return Boolean.class;
-        } else if ("text".equals(type)) {
-            return String.class;
-        } else if ("bytea".equals(type)) {
-            return byte[].class;
-        } else {
-            new Error("Not suportend type: " + type);
-        }
-
-        return null;
-    }
-
-    public synchronized static PreparedStatement preparedStatementUpdate(String schema, Connection connection, List<ModelTableField> modelTableFields,
+    public static PreparedStatement preparedStatementUpdate(String schema, Connection connection, List<Model> models,
                                                             String tableName, Long id) throws Exception {
 
         StringBuilder values = new StringBuilder();
-        for (ModelTableField modelTableField : modelTableFields) {
+        for (Model model : models) {
 
-            if (modelTableField.getField().isAnnotationPresent(Json.class)) {
-                values.append(modelTableField.getAttribute() + "= ?::JSON,");
+            if (model.getField().isAnnotationPresent(Json.class)) {
+
+                Json json = (Json)model.getField().getAnnotation(Json.class);
+
+                values.append(model.getAttribute() + "= ?::"+json.typeJson()+",");
             } else {
-                values.append(modelTableField.getAttribute() + "= ?,");
+                values.append(model.getAttribute() + "= ?,");
             }
 
         }
@@ -254,32 +109,7 @@ public class SqlUtil {
 
         PreparedStatement ps = connection.prepareStatement(sql);
 
-        return getPreparedStatement(ps, modelTableFields);
-
-    }
-
-    public static synchronized boolean isTableExist(Connection connection, String schema, String table) {
-
-        Statement stmt = null;
-        try {
-            stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT to_regclass('"+schema+"."+table+"') is not null");
-
-            if (rs.next()) {
-                return rs.getBoolean(1);
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                stmt.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-
-        return false;
+        return getPreparedStatement(ps, models);
 
     }
 
